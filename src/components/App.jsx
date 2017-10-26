9d9bf84d155b4db16634987c6b6e0252005046b1
Hack Reactor Student <student@hackreactor.com>
2017-10-25 17:58:22 -0700

    populate video player and video list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,21 @@
-var App = () => (
-  <div>
+
+// import React from 'react';
+
+class App extends React.Component {
+//constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      videolist : window.exampleVideoData,
+      video: window.exampleVideoData[0],   
+    };
+  }
+  
+//render 
+
+  render() {
+    return (
+     <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
         <div><h5><em>search</em> view goes here</h5></div>
@@ -7,15 +23,44 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <VideoPlayer video={this.state.video}/>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <VideoList videos={this.state.videolist}/>
       </div>
     </div>
   </div>
-);
+    );
+  }
+}
+
+
+window.App = App;
+
+
+
+
+
+ 
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-window.App = App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
